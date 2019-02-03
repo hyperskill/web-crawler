17e5d60ca7a58c5dd5abd71d9a4513d0507a83ad
Sergei Khmelevskiy <xmelko91@gmail.com>
2019-02-03 12:35:45 +0300

    simple window

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -1,12 +1,25 @@
 package webcrawler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        InContent textCont = new InContent(200,200);
+        setLayout(new GroupLayout(getContentPane()));
+        setTitle("WebCrawler");
+        add(textCont.textArea).setLocation(30,30);
         setVisible(true);
         setLayout(null);
+    }
+}
+
+class InContent extends JFrame {
+    JTextArea textArea = new JTextArea("HTML CODE??");
+
+    InContent(int x, int y){
+        textArea.setSize(x, y);
     }
 }
