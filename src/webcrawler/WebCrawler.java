f0c398ff0ea2a8750b0d4af414cda9e899bc4b95
Valkryst <valkryst@valkryst.com>
2023-11-02 11:43:08 -0300

    Rewrite `WebCrawler`'s constructor

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -1,12 +1,18 @@
 package webcrawler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
-        setVisible(true);
-        setLayout(null);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setPreferredSize(new Dimension(512, 512));
+        super.setTitle("Web Crawler");
+
+        // Create components and add them to this JFrame.
+
+        super.setVisible(true);
+        super.pack();
+        super.setLocationRelativeTo(null);
     }
 }
